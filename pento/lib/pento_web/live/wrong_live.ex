af7ed68935264a21d6fd015daea10a15c42e2299
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-07 14:26:11 -0500

    LiveView 1b
    updated time in mount, render & handle_event to react in real time to guess click

--- pento/lib/pento_web/live/wrong_live.ex
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -3,7 +3,7 @@ defmodule PentoWeb.WrongLive do
     "live.html"}
 
     def mount(_params, _session, socket) do
-        {:ok, assign(socket, score: 0, message: "Make a guess")}
+        {:ok, assign(socket, score: 0, message: "Make a guess", time: DateTime.utc_now)}
     end  
 
     def render(assigns) do
@@ -17,19 +17,30 @@ defmodule PentoWeb.WrongLive do
             <a href="#" phx-click="guess" phx-value-number= {n} > <%= n %></a>
             <% end %>
         </h2>
+
+        <h2>
+            <%= @message %>
+            It's <%= @time %>
+        </h2>    
         """
     end 
+
+    def time() do
+        DateTime.utc_now |> to_string
+    end    
 
     def handle_event("guess", %{"number" => guess}=data, socket)
     do
         message = "Your guess: #{guess}. Wrong. Guess again. "
         score = socket.assigns.score - 1
+        time = DateTime.utc_now
 
         {
             :noreply,
             assign(
                 socket,
                 message: message,
-                score: score)}
+                score: score,
+                time: time)}
     end                         
 end    
