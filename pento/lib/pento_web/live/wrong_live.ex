d4d3166023e3d5a17516f8b3b5cd8b8c3627cdee
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-07 21:41:32 -0500

    LiveView 1d
    semantic & otherwise cleanup

--- pento/lib/pento_web/live/wrong_live.ex
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -3,7 +3,13 @@ defmodule PentoWeb.WrongLive do
     "live.html"}
 
     def mount(_params, _session, socket) do
-        {:ok, assign(socket, score: 0, message: "Make a guess", time: DateTime.utc_now, correct_guess: Enum.random(1..10) |> to_string())}
+        {
+            :ok, assign(socket, 
+            score: 0, 
+            message: "Make a guess! ", 
+            time: DateTime.utc_now, 
+            system_guess: Enum.random(1..10) |> to_string()
+        )}
     end  
 
     def render(assigns) do
@@ -21,16 +27,16 @@ defmodule PentoWeb.WrongLive do
         <h2>
             <%= @message %>
             It's <%= @time %>
-        </h2>    
+        </h2>
+  
         """
     end 
 
-    def handle_event("guess",%{"number" => guess} = data, %{assigns: %{correct_guess: guess}} = socket)
+    def handle_event("guess",%{"number" => guess} = data, %{assigns: %{system_guess: guess}} = socket)
     do   
             winning_message = "Your guess: #{guess} is SUPER CORRECT. WINNER!!!! "
             score = socket.assigns.score + 1
             time = DateTime.utc_now
-
         {
             :noreply,
             assign(
@@ -38,10 +44,10 @@ defmodule PentoWeb.WrongLive do
                 message: winning_message,
                 score: score,
                 time: time,
-                )}
+        )}
     end
 
-    def handle_event("guess",%{"number" => guess}=data,%{assigns: %{correct_guess: correct_guess}} = socket)
+    def handle_event("guess",%{"number" => guess}=data,%{assigns: %{system_guess: system_guess}} = socket)
     do
             losing_message = "Your guess: #{guess}. Wrong. Guess again. "
             score = socket.assigns.score - 1
@@ -54,7 +60,6 @@ defmodule PentoWeb.WrongLive do
                 message: losing_message,
                 score: score,
                 time: time,
-                )}
-    end
-                           
+        )}
+    end              
 end    
