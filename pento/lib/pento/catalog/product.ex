1774bc38b987830ba99317178843d70f494362b8
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-09 18:29:24 -0500

    CoreLayer 1a
    updated product schema to only accept unit price > 0.0

--- pento/lib/pento/catalog/product.ex
+++ pento/lib/pento/catalog/product.ex
@@ -17,5 +17,6 @@ defmodule Pento.Catalog.Product do
     |> cast(attrs, [:name, :description, :unit_price, :sku])
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
+    |> validate_number(:unit_price, greater_than: 0.0)
   end
 end
