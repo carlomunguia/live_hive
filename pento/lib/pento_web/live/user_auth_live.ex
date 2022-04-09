56e8ba29eab541627cd77dad22e0efd01afdcbde
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-09 17:34:21 -0500

    AuthLayer3
    optimized socket assign to no longer need additional db calls if current user & in disconnected plug state

--- pento/lib/pento_web/live/user_auth_live.ex
+++ pento/lib/pento_web/live/user_auth_live.ex
@@ -4,7 +4,7 @@ defmodule PentoWeb.UserAuthLive do
 
     def on_mount(_, params, %{"user_token" => user_token}, socket) do
         user = Accounts.get_user_by_session_token(user_token)
-        socket = socket |> assign(:current_user, user)
+        socket = assign_new(socket, :current_user, fn -> Accounts.get_user!(user.id) end)
         
         if socket.assigns.current_user do
             {:cont, socket}
