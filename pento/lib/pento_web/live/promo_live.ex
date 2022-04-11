0f9ed906bf26ef4379ef351968bd1f048eb7cc03
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-11 14:28:33 -0500

    Rolled Layer 2
    created promo live view with mount, assigns for recipient & changeset, and the handle_event function for validation, also created heex template, updated router file for new promo route

---
+++ pento/lib/pento_web/live/promo_live.ex
@@ -0,0 +1,37 @@
+defmodule PentoWeb.PromoLive do
+  use PentoWeb, :live_view
+  alias Pento.Promo
+  alias Pento.Promo.Recipient
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign_recipient()
+     |> assign_changeset()}
+  end
+
+  def assign_recipient(socket) do
+    socket
+    |> assign(:recipient, %Recipient{})
+  end
+
+  def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
+    socket
+    |> assign(:changeset, Promo.change_recipient(recipient))
+  end
+
+  def handle_event(
+        "validate",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    changeset =
+      recipient
+      |> Promo.change_recipient(recipient_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply,
+     socket
+     |> assign(:changeset, changeset)}
+  end
+end
