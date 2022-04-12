17611c521c768a81c458ad7d3d9129877ad6339a
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-11 22:20:54 -0500

    Rolled Layer 3
    created custom image upload functionality, updated form_component.ex to accommodate the change, updated form component & show heex files to render image uploads, added progress bar for image, did data migration for :image_upload to product table

--- pento/lib/pento_web/live/product_live/form_component.ex
+++ pento/lib/pento_web/live/product_live/form_component.ex
@@ -6,11 +6,19 @@ defmodule PentoWeb.ProductLive.FormComponent do
   @impl true
   def update(%{product: product} = assigns, socket) do
     changeset = Catalog.change_product(product)
+    Process.sleep(250)
 
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> allow_upload(:image,
+       accept: ~w(.jpg .jpeg .png),
+       max_entries: 1,
+       max_file_size: 9_000_000,
+       auto_upload: true,
+       progress: &handle_progress/3
+     )}
   end
 
   @impl true
@@ -27,8 +35,10 @@ defmodule PentoWeb.ProductLive.FormComponent do
     save_product(socket, socket.assigns.action, product_params)
   end
 
-  defp save_product(socket, :edit, product_params) do
-    case Catalog.update_product(socket.assigns.product, product_params) do
+  defp save_product(socket, :edit, params) do
+    result = Catalog.update_product(socket.assigns.product, product_params(socket, params))
+
+    case result do
       {:ok, _product} ->
         {:noreply,
          socket
@@ -40,8 +50,8 @@ defmodule PentoWeb.ProductLive.FormComponent do
     end
   end
 
-  defp save_product(socket, :new, product_params) do
-    case Catalog.create_product(product_params) do
+  defp save_product(socket, :new, params) do
+    case Catalog.create_product(product_params(socket, params)) do
       {:ok, _product} ->
         {:noreply,
          socket
@@ -51,5 +61,35 @@ defmodule PentoWeb.ProductLive.FormComponent do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  defp handle_progress(:image, entry, socket) do
+    :timer.sleep(1000)
+
+    if entry.done? do
+      path =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+
+      {:noreply,
+       socket
+       |> put_flash(:info, "file #{entry.client_name} uploaded")
+       |> assign(:image_upload, path)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  defp upload_static_file(%{path: path}, socket) do
+    dest = Path.join("priv/static/images", Path.basename(path))
+    File.cp!(path, dest)
+    {:ok, Routes.static_path(socket, "/images/#{Path.basename(dest)}")}
+  end
+
+  def product_params(socket, params) do
+    Map.put(params, "image_upload", socket.assigns.image_upload)
   end
 end
