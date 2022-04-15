4c3d438059ba48e928d599be05131b203da02beb
Carlo Munguia <cmunguia@denimgroup.com>
2022-04-15 10:10:54 -0500

    Custom Context 1b
    the rating query.ex somehow didn't make it over the wire

---
+++ pento/lib/pento/survey/rating/query.ex
@@ -0,0 +1,19 @@
+defmodule Pento.Survey.Rating.Query do
+  import Ecto.Query
+
+  alias Pento.Survey.Rating
+
+  def base do
+    Rating
+  end
+
+  def preload_user(user) do
+    base()
+    |> for_user(user)
+  end
+
+  defp for_user(query, user) do
+    query
+    |> where([r], r.user_id == ^user.id)
+  end
+end
